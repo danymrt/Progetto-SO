862118fd019825b7671d155af0259a4718a91563
francescosche <francesco.schettino95@gmail.com>
2019-05-08 18:28:03 +0200

    Accorpate tutte le funzioni nel file C e iniziato lo sviluppo della prima funzione

---
+++ project/simplefs_test.c
@@ -0,0 +1,142 @@
+#include "simplefs.h"
+#include "disk_driver.h"
+#include "bitmap.h"
+#include <stdio.h>
+
+int main(int agc, char** argv) {
+	printf("FirstBlock size %ld\n", sizeof(FirstFileBlock));
+	printf("DataBlock size %ld\n", sizeof(FileBlock));
+	printf("FirstDirectoryBlock size %ld\n", sizeof(FirstDirectoryBlock));
+	printf("DirectoryBlock size %ld\n", sizeof(DirectoryBlock));  
+}
+
+// converte l'indice del blocco in due elementi: un indice dell'array e un carattere che mostra la posizione del bit all'interno dell'array stesso
+// converts a block index to an index in the array, and a char that indicates the offset of the bit inside the array
+BitMapEntryKey BitMap_blockToIndex(int num) {
+	// definiamo il blocco da restituire
+	BitMapEntryKey blocco;
+	// 
+	blocco->entry_num = ???; // int
+	blocco->bit_num = ???; // char 00000100
+	return blocco;
+}
+
+/*
+// converts a bit to a linear index
+int BitMap_indexToBlock(int entry, uint8_t bit_num) {
+}
+
+// returns the index of the first bit having status "status"
+// in the bitmap bmap, and starts looking from position start
+int BitMap_get(BitMap* bmap, int start, int status) {
+}
+
+// sets the bit at index pos in bmap to status
+int BitMap_set(BitMap* bmap, int pos, int status) {
+}
+
+
+// The blocks indices seen by the read/write functions 
+// have to be calculated after the space occupied by the bitmap
+
+// opens the file (creating it if necessary_
+// allocates the necessary space on the disk
+// calculates how big the bitmap should be
+// if the file was new
+// compiles a disk header, and fills in the bitmap of appropriate size
+// with all 0 (to denote the free space);
+void DiskDriver_init(DiskDriver* disk, const char* filename, int num_blocks) {
+}
+
+// reads the block in position block_num
+// returns -1 if the block is free accrding to the bitmap
+// 0 otherwise
+int DiskDriver_readBlock(DiskDriver* disk, void* dest, int block_num) {
+}
+
+// writes a block in position block_num, and alters the bitmap accordingly
+// returns -1 if operation not possible
+int DiskDriver_writeBlock(DiskDriver* disk, void* src, int block_num) {
+}
+
+// frees a block in position block_num, and alters the bitmap accordingly
+// returns -1 if operation not possible
+int DiskDriver_freeBlock(DiskDriver* disk, int block_num) {
+}
+
+// returns the first free blockin the disk from position (checking the bitmap)
+int DiskDriver_getFreeBlock(DiskDriver* disk, int start) {
+}
+
+// writes the data (flushing the mmaps)
+int DiskDriver_flush(DiskDriver* disk) {
+}
+
+// initializes a file system on an already made disk
+// returns a handle to the top level directory stored in the first block
+DirectoryHandle* SimpleFS_init(SimpleFS* fs, DiskDriver* disk) {
+
+}
+
+// creates the inital structures, the top level directory
+// has name "/" and its control block is in the first position
+// it also clears the bitmap of occupied blocks on the disk
+// the current_directory_block is cached in the SimpleFS struct
+// and set to the top level directory
+void SimpleFS_format(SimpleFS* fs) {
+}
+
+// creates an empty file in the directory d
+// returns null on error (file existing, no free blocks)
+// an empty file consists only of a block of type FirstBlock
+FileHandle* SimpleFS_createFile(DirectoryHandle* d, const char* filename) {
+}
+
+// reads in the (preallocated) blocks array, the name of all files in a directory 
+int SimpleFS_readDir(char** names, DirectoryHandle* d) {
+}
+
+// opens a file in the  directory d. The file should be exisiting
+FileHandle* SimpleFS_openFile(DirectoryHandle* d, const char* filename) {
+}
+
+// closes a file handle (destroyes it)
+int SimpleFS_close(FileHandle* f) {
+}
+
+// writes in the file, at current position for size bytes stored in data
+// overwriting and allocating new space if necessary
+// returns the number of bytes written
+int SimpleFS_write(FileHandle* f, void* data, int size) {
+}
+
+// writes in the file, at current position size bytes stored in data
+// overwriting and allocating new space if necessary
+// returns the number of bytes read
+int SimpleFS_read(FileHandle* f, void* data, int size) {
+}
+
+// returns the number of bytes read (moving the current pointer to pos)
+// returns pos on success
+// -1 on error (file too short)
+int SimpleFS_seek(FileHandle* f, int pos) {
+}
+
+// seeks for a directory in d. If dirname is equal to ".." it goes one level up
+// 0 on success, negative value on error
+// it does side effect on the provided handle
+ int SimpleFS_changeDir(DirectoryHandle* d, char* dirname) {
+}
+
+// creates a new directory in the current one (stored in fs->current_directory_block)
+// 0 on success
+// -1 on error
+int SimpleFS_mkDir(DirectoryHandle* d, char* dirname) {
+}
+
+// removes the file in the current directory
+// returns -1 on failure 0 on success
+// if a directory, it removes recursively all contained files
+int SimpleFS_remove(SimpleFS* fs, char* filename) {
+}
+*/
