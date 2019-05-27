20a9549554e74cc4dc0d976f2d4b5870d748b77c
francescosche <francesco.schettino95@gmail.com>
2019-05-27 11:17:41 +0200

    Modifiche al test

--- project/simplefs_test.c
+++ project/simplefs_test.c
@@ -20,26 +20,27 @@ int main(int agc, char** argv) {
 	// printf("DirectoryBlock size %ld\n", sizeof(DirectoryBlock));
 
 	// Test BitMap_create
-	printf("\n+++ Test BitMap_create()");
+	printf("\n+++ Test DiskDriver_init() e, all'interno, BitMap_init()");
 	DiskDriver disk;
 	char* filename = "test.txt";
 	int num_blocks = 100;
 	DiskDriver_init(&disk, filename, num_blocks);
 	BitMap bitmap=*(disk.bitmap);
+	printf("\n    BitMap creata e inizializzata correttamente");
 
 	// Test BitMap_blockToIndex
-	printf("\n+++ Test BitMap_blockToIndex()");
 	int num = 4 * BLOCK_SIZE;
+	printf("\n\n+++ Test BitMap_blockToIndex(%d)", num);
 	BitMapEntryKey block = BitMap_blockToIndex(num);
-	printf("\n\nLa posizione del blocco è %d, ovvero entry %d con sfasamento %d", num, block.entry_num, block.bit_num);
+	printf("\n    La posizione del blocco è %d, ovvero entry %d con sfasamento %d", num, block.entry_num, block.bit_num);
 
 	// Test BitMap_indexToBlock
-	printf("\n+++ Test BitMap_indexToBlock()");
+	printf("\n\n+++ Test BitMap_indexToBlock(block)");
 	int posizione = BitMap_indexToBlock(block);
-	printf("\n\nAbbiamo la entry %d e lo sfasamento %d, ovvero la posizione %d", block.entry_num, block.bit_num, posizione);
+	printf("\n    Abbiamo la entry %d e lo sfasamento %d, ovvero la posizione %d", block.entry_num, block.bit_num, posizione);
 
 	// Test BitMap_set
-	printf("\n+++ Test BitMap_set()");
+	printf("\n\n+++ Test BitMap_set()");
 	BitMap* bimap = (BitMap*)malloc(sizeof(BitMap));
   bimap ->num_bits = 32;
   bimap ->entries = (char*)malloc(sizeof(char) * 4);
@@ -74,48 +75,51 @@ int main(int agc, char** argv) {
 	for(i=0; i<= (bimap ->num_bits)/8; i++){
 		printf("%c",bimap ->entries[i]);
 	}
-  printf("\n\n");
+  printf("\n");
 
 
 	// Test BitMap_get
 	printf("\n+++ Test BitMap_get()");
-	for(i=0; i<= (bimap ->num_bits)/8; i++){
-		printf("%c",bimap ->entries[i]);
+	printf("\n    bimap->entries contiene: ");
+	for(i = 0; i <= ( bimap->num_bits ) / 8; i++){
+		printf("%c", bimap->entries[i]);
 	}
+	printf("\n    bimap->entries contiene, in binario: "); // TODO: Modificare la stampa in binario
 	printf("\n");
 	int start = 6;
 	int status = 0;
-	printf("Verifichiamo con start=%d e status=%d ... %d\n", start, status, BitMap_get(bimap, start, status));
+	printf("    Partiamo dalla posizione %d e cerchiamo %d => %d\n", start, status, BitMap_get(bimap, start, status));
 	start = 3;
 	status = 1;
-	printf("Verifichiamo con start=%d e status=%d ... %d\n", start, status, BitMap_get(bimap, start, status));
+	printf("    Partiamo dalla posizione %d e cerchiamo %d => %d\n", start, status, BitMap_get(bimap, start, status));
 	start = 12;
 	status = 0;
-	printf("Verifichiamo con start=%d e status=%d ... %d\n", start, status, BitMap_get(bimap, start, status));
+	printf("    Partiamo dalla posizione %d e cerchiamo %d => %d\n", start, status, BitMap_get(bimap, start, status));
 	start = 13;
 	status = 1;
-	printf("Verifichiamo con start=%d e status=%d ... %d\n", start, status, BitMap_get(bimap, start, status));
+	printf("    Partiamo dalla posizione %d e cerchiamo %d => %d\n", start, status, BitMap_get(bimap, start, status));
 
-
+	/*
 	// Test DiskDriver_writeBlock
 	printf("\n+++ Test DiskDriver_writeBlock()");
 	int block_num=0;
-	void* src="hello geek";
-	printf("Il risultato della write con block_num=%d è: %d\n", block_num, DiskDriver_writeBlock(&disk,src,block_num));
+	void* src = "ciao mondo";
+	printf("\n    Il risultato della write con block_num=%d è: %d\n", block_num, DiskDriver_writeBlock(&disk,src,block_num));
 
 
 	// Test DiskDriver_readBlock
 	printf("\n+++ Test DiskDriver_readBlock()");
 	void* dest;
-	printf("Il risultato della read con block_num=%d è: %d\n", block_num, DiskDriver_readBlock(&disk,dest,block_num));
+	printf("\n    Il risultato della read con block_num=%d è: %d\n", block_num, DiskDriver_readBlock(&disk,dest,block_num));
 
 
 	// Test DiskDriver_freeBlock
 	printf("\n+++ Test DiskDriver_freeBlock()");
-	//printf("\n Il risultato della FreeBlock è: %d \n",DiskDriver_freeBlock(&disk,0));
+	printf("\n Il risultato della FreeBlock è: %d \n",DiskDriver_freeBlock(&disk,0));
 
 
 	// Test DiskDriver_getFreeBlock
 	printf("\n+++ Test DiskDriver_getFreeBlock()");
-	printf("\n Il risultato della getFreeBlock è: %d \n",DiskDriver_getFreeBlock(&disk,0));
+	printf("\n    Il risultato della getFreeBlock è: %d \n",DiskDriver_getFreeBlock(&disk,0));
+	*/
 }
